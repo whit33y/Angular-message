25190f6b68eae62d555b3f394f4f58f0747339d1
whit33y <troppout@gmail.com>
2024-10-13 13:41:48 +0200

    Created chat card component with styles

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ChatCardComponent } from '../../components/chat-card/chat-card.component';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [ChatCardComponent],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
