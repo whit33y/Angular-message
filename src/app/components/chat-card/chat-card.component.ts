69f99488bb3310571c5b413f7db17f61c680c5da
whit33y <troppout@gmail.com>
2024-10-14 07:26:42 +0200

    Chat card update css and new component generated

--- src/app/components/chat-card/chat-card.component.ts
+++ src/app/components/chat-card/chat-card.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-chat-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './chat-card.component.html',
   styleUrl: './chat-card.component.css'
 })
 export class ChatCardComponent {
+  @Input() author: string = 'author';
 
 }
