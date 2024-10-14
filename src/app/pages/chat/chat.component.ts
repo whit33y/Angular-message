e8a9d0a27684a10d529c7350bc0d901474004133
whit33y <troppout@gmail.com>
2024-10-14 07:34:46 +0200

    Added basic styles on chat input

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { ChatCardComponent } from '../../components/chat-card/chat-card.component';
+import { ChatInputComponent } from '../../components/chat-input/chat-input.component';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [ChatCardComponent],
+  imports: [ChatCardComponent, ChatInputComponent],
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.css'
+  styleUrl: './chat.component.css',
 })
-export class ChatComponent {
-
-}
+export class ChatComponent {}
