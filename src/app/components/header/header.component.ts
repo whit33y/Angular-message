037edc75df328ea61bb928477a1b5801b8b2f9d0
whit33y <troppout@gmail.com>
2024-11-09 21:28:30 +0100

    added authguard and logout function

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  private auth = inject(AuthService);
 
+  constructor(private router: Router) {}
+
+  isUserLoggedIn = false;
+  ngOnInit() {
+    this.isUserLoggedIn = this.auth.isLoggedIn;
+  }
+
+  async logout() {
+    this.auth
+      .signOut()
+      .then(() => {
+        localStorage.removeItem('session');
+        this.router.navigate(['/login']);
+        this.isUserLoggedIn = false;
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,30 +1,43 @@
 import { Injectable } from '@angular/core';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 import { environment } from '../../../environment';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   private supabase!: SupabaseClient;
-  constructor() { 
+  constructor(private router: Router) {
     this.supabase = createClient(
       environment.supabaseUrl,
       environment.supabaseKey
-    )
-    this.supabase.auth.onAuthStateChange((event,session)=> {
-      console.log('event', event);
-      console.log('session', session)
-    })
+    );
+    this.supabase.auth.onAuthStateChange((event, session) => {
+      // console.log('event', event);
+      // console.log('session', session);
+      if (session) {
+        localStorage.setItem('session', JSON.stringify(session?.user));
+      }
+
+      if (session?.user) {
+        this.router.navigate(['/chat']);
+      }
+    });
   }
 
-  async signInWithGoogle(){
+  get isLoggedIn(): boolean {
+    const user = localStorage.getItem('session') as string;
+    return user === 'undefined' ? false : true;
+  }
+
+  async signInWithGoogle() {
     await this.supabase.auth.signInWithOAuth({
       provider: 'google',
-    })
-  }  
+    });
+  }
 
-  async signOut(){
-    await this.supabase.auth.signOut()
+  async signOut() {
+    await this.supabase.auth.signOut();
   }
 }
